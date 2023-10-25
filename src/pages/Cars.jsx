809ca8164b2850fbe450c8f17532071865a44bde
ticoniq_ <okolietochi@gmail>
2023-10-25 18:37:08 +0100

    updated

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Carousel from "react-elastic-carousel"
-import Item from "../components/Item";
 import mini from "../assets/images/mini.png";
 import retro from "../assets/images/retro.png";
 import small from "../assets/images/small.png";
@@ -17,7 +15,7 @@ function Cars() {
     <section className="my-16 m-auto md:my-20 p-4">
       <h1 className="text-center text-4xl font-extrabold">LATEST MODELS</h1>
       <p className="text-center text-gray-500">Please select a car model</p>
-      <Carousel breakPoints={breakPoints}>
+      {/* <Carousel breakPoints={breakPoints}>
         <Item>
           <div className='relative bg-gray-200 rounded-full w-48 h-48 my-20 mx-auto md:h-72 md:w-72'>
             <img src={mini} className="h-full object-fill" alt={mini} />
@@ -48,7 +46,7 @@ function Cars() {
             <img src={small} className="h-full object-fill" alt={small} />
           </div>
         </Item>
-      </Carousel>
+      </Carousel> */}
     </section>
   );
 }
