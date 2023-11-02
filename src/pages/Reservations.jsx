1b54956cb9838de8cd1520b4c6fd8f9ec4b94985
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-11-02 14:26:37 +0500

    Modify Reservation File

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -82,7 +82,77 @@ function Reservations() {
     setAlert('');
   }, 3000);
 
-  
+  return (
+    <>
+      {reservationData ? (
+        <section className="relative inset-0 h-full flex justify-center items-center">
+          <div className="p-5 max-w-5xl">
+            <h2 className="text-center text-3xl font-bold">Reservations</h2>
+            {alert && <Alert msg={alert} />}
+            <div className="overflow-x-auto">
+              <table className="table my-10 md:my-20">
+                {/* head */}
+                <thead>
+                  <tr>
+                    <th className="text-2xl">
+                      Car Model
+                    </th>
+                    <th className="text-2xl">Image</th>
+                    <th className="text-2xl">Start Date</th>
+                    <th className="text-2xl">End date</th>
+                    <th />
+                  </tr>
+                </thead>
+                <tbody>
+                  {reservationData.data.map((reservation) => (
+                    <tr key={reservation.id}>
+                      <th>
+                        {cars.find((car) => car.id === reservation.car_id)?.model}
+                      </th>
+                      <td>
+                        <div className="flex items-center space-x-3">
+                          <div className="avatar">
+                            <div className="mask mask-squircle w-20 h-12">
+                              <img
+                                className="w-full object-contain"
+                                src={cars.find((car) => car.id === reservation.car_id)?.picture}
+                                alt={cars.find((car) => car.id === reservation.car_id)?.model}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td>{formatTimestamp(reservation.start_date)}</td>
+                      <td>{formatTimestamp(reservation.end_date)}</td>
+                      <th>
+                        <Link
+                          className="btn btn-ghost btn-xs"
+                          to={`/car_details/${reservation.car_id}`}
+                        >
+                          Details
+                        </Link>
+                        <button
+                          type="button"
+                          className="btn btn-ghost btn-xs"
+                          onClick={() => handleDelete(reservation.id)}
+                        >
+                          Delete
+                        </button>
+                      </th>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <div className="flex justify-center items-center">
+          <span className="h-screen loading loading-bars loading-lg" />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Reservations;
